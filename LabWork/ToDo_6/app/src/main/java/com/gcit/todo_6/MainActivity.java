6335b717b93570d1af45311f22b01e31db6e0a0d
Jurmey1753 <tsheringjurmey74@gmail.com>
2021-03-23 18:33:43 +0600

    ToDo_6

---
+++ LabWork/ToDo_6/app/src/main/java/com/gcit/todo_6/MainActivity.java
@@ -0,0 +1,71 @@
+package com.gcit.todo_6;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   // private static final String LOG_TAG =MainActivity.class.getSimpleName();
+    public final static String EXTRA_MESSAGE = "com.gcit.todo_6.MESSAGE";
+    private static final int TEXT_REQUEST = 1;
+    private EditText editText;
+    private TextView Tv1;
+    private TextView Tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Log.d(LOG_TAG, "-------");
+//        Log.d(LOG_TAG, "onCreate");
+
+        editText = findViewById(R.id.editText1);
+        Tv1 = findViewById(R.id.Received_Reply);
+        Tv2 = findViewById(R.id.Replied_Message);
+
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState.getBoolean("reply_state");
+            if (isVisible) {
+                Tv1.setVisibility(View.VISIBLE);
+                Tv2.setText(savedInstanceState.getString("reply_MESSAGE"));
+                Tv2.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void SEND(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivityForResult(intent,TEXT_REQUEST);
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent obj) {
+        super.onActivityResult(requestCode, resultCode, obj);
+        if(requestCode == TEXT_REQUEST){
+            if(resultCode == RESULT_OK){
+                String message = obj.getStringExtra(MainActivity2.EXTRA_REPLY);
+                Tv1.setVisibility(View.VISIBLE);
+                Tv2.setText(message);
+                Tv2.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (Tv1.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_state", true);
+            outState.putString("reply_MESSAGE", Tv2.getText().toString());
+        }
+    }
+}
